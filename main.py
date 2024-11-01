0927043371945a450b5c622de943d79358eae763
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-11-01 23:17:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ client = discord.Client()
 TOKEN = os.getenv("TOKEN")
 
 # もし退出したくないグループDMがある場合はここにIDを書いておく
-# EXCLUDE_DM_IDS = ["", ""] 
+# DM_IDS = ["", ""] 
 
 @client.event
 async def on_ready():
@@ -16,8 +16,8 @@ async def on_ready():
     group_dms = [dm for dm in client.private_channels if isinstance(dm, discord.GroupChannel)]
     left_count = 0
     for dm in group_dms:
-      # EXCLUDE_DM_IDSを入力した場合はこれを書いておく
-      # if str(dm.id) not in EXCLUDE_DM_IDS:
+      # DM_IDSを入力した場合はこれを書いておく
+      # if str(dm.id) not in DM_IDS:
             await dm.leave()
             left_count += 1
     print(f"{left_count}件のグループDMからの退出が完了しました")
