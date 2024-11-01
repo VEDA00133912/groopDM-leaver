4a7d03cbb521bc24e9492b0ba1aab5c7f1e6a54c
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-11-01 23:02:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ client = discord.Client()
 
 TOKEN = os.getenv("TOKEN")
 
+EXCLUDE_DM_IDS = ["", ""] 
+
 @client.event
 async def on_ready():
     print(f"{client.user} is online！")
     group_dms = [dm for dm in client.private_channels if isinstance(dm, discord.GroupChannel)]
+    left_count = 0
     for dm in group_dms:
-        await dm.leave()
-    print(f"{len(group_dms)}件のグループDMからの退出が完了しました")
+        if str(dm.id) not in EXCLUDE_DM_IDS:
+            await dm.leave()
+            left_count += 1
+    print(f"{left_count}件のグループDMからの退出が完了しました")
 
 client.run(TOKEN)
